8ce02cbaafe8a2518e8f05704ccc566d75042c66
yamachi <yamachig@users.noreply.github.com>
2022-04-06 22:51:32 -0700

    validateを追加

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,22 +1,31 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 import * as fs from "fs";
 import { parse } from "lawtext/dist/src/parser/lawtext";
 import * as renderer from "lawtext/dist/src/renderer";
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    TransportKind
+} from "vscode-languageclient/node";
+import path = require("path");
 
-
-const renderActiveEditor = () => {
+const renderActiveEditor = (context: vscode.ExtensionContext) => {
     const editor = vscode.window.activeTextEditor;
     if (!editor) return "error";
-    if (!(editor.document.languageId === 'lawtext')) {
+    if (!(editor.document.languageId === "lawtext")) {
         return "Active editor doesn't show a Lawtext document.";
     }
     const lawtext = editor.document.getText();
-    const {value:law} = parse(lawtext);
+    const { value: law } = parse(lawtext);
+    const cssFile = context.asAbsolutePath(
+        path.join("client", "src", "static", "law.css")
+    );
     const rendered = renderer.renderHtml(
         law,
         {
             style: [
-                fs.readFileSync(__dirname + '/../src/static/law.css', { encoding: "utf-8" }),
+                fs.readFileSync(cssFile, { encoding: "utf-8" }),
                 "body { background-color: white; color: black; }"
             ].join("\n"),
         }
@@ -24,17 +33,58 @@ const renderActiveEditor = () => {
     return rendered;
 };
 
+let client: LanguageClient;
+
 export const activate = (context: vscode.ExtensionContext) => {
     context.subscriptions.push(
-        vscode.commands.registerCommand('extension.showLawtextPreview', () => {
+        vscode.commands.registerCommand("extension.showLawtextPreview", () => {
             const panel = vscode.window.createWebviewPanel(
-                'lawtextPreview',
-                'Lawtext Preview',
+                "lawtextPreview",
+                "Lawtext Preview",
                 vscode.ViewColumn.Two,
                 {},
             );
 
-            panel.webview.html = renderActiveEditor();
+            panel.webview.html = renderActiveEditor(context);
         })
     );
+
+    const serverModule = context.asAbsolutePath(
+        path.join("server", "out", "server.js")
+    );
+
+    const debugOptions = { execArgv: ["--nolazy", "--inspect=6000"] };
+
+    const serverOptions: ServerOptions = {
+        run: { module: serverModule, transport: TransportKind.ipc },
+        debug: {
+            module: serverModule,
+            transport: TransportKind.ipc,
+            options: debugOptions
+        }
+    };
+
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: "file", language: "lawtext" }],
+        synchronize: {
+            fileEvents: vscode.workspace.createFileSystemWatcher("**/.clientrc")
+        }
+    };
+
+    client = new LanguageClient(
+        "lawtextLanguageServe",
+        "Lawtext Language Server",
+        serverOptions,
+        clientOptions
+    );
+
+    client.start();
+};
+
+
+export function deactivate(): Thenable<void> | undefined {
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
 }
---
+++ server/src/server.ts
@@ -0,0 +1,111 @@
+import {
+    createConnection,
+    TextDocuments,
+    Diagnostic,
+    DiagnosticSeverity,
+    ProposedFeatures,
+    InitializeParams,
+    DidChangeConfigurationNotification,
+    TextDocumentSyncKind,
+    InitializeResult
+} from "vscode-languageserver/node";
+
+import {
+    TextDocument
+} from "vscode-languageserver-textdocument";
+
+import { parse } from "lawtext/dist/src/parser/lawtext";
+
+const connection = createConnection(ProposedFeatures.all);
+
+const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+let hasConfigurationCapability = false;
+let hasWorkspaceFolderCapability = false;
+
+connection.onInitialize((params: InitializeParams) => {
+    const capabilities = params.capabilities;
+
+    hasConfigurationCapability = !!(
+        capabilities.workspace && !!capabilities.workspace.configuration
+    );
+    hasWorkspaceFolderCapability = !!(
+        capabilities.workspace && !!capabilities.workspace.workspaceFolders
+    );
+
+    const result: InitializeResult = {
+        capabilities: {
+            textDocumentSync: TextDocumentSyncKind.Incremental
+        }
+    };
+    if (hasWorkspaceFolderCapability) {
+        result.capabilities.workspace = {
+            workspaceFolders: {
+                supported: true
+            }
+        };
+    }
+    return result;
+});
+
+connection.onInitialized(() => {
+    if (hasConfigurationCapability) {
+        connection.client.register(DidChangeConfigurationNotification.type, undefined);
+    }
+    if (hasWorkspaceFolderCapability) {
+        connection.workspace.onDidChangeWorkspaceFolders(() => {
+            connection.console.log("Workspace folder change event received.");
+        });
+    }
+});
+
+interface ExampleSettings {
+	maxNumberOfProblems: number;
+}
+
+const documentSettings: Map<string, Thenable<ExampleSettings>> = new Map();
+
+connection.onDidChangeConfiguration(() => {
+    if (hasConfigurationCapability) {
+        documentSettings.clear();
+    }
+
+    documents.all().forEach(validate);
+});
+
+documents.onDidClose(e => {
+    documentSettings.delete(e.document.uri);
+});
+
+documents.onDidChangeContent(change => {
+    validate(change.document);
+});
+
+const validate = async (textDocument: TextDocument): Promise<void> => {
+    const lawtext = textDocument.getText();
+    const { errors } = parse(lawtext);
+
+    const diagnostics: Diagnostic[] = [];
+    for (const error of errors) {
+        const diagnostic: Diagnostic = {
+            severity: DiagnosticSeverity.Error,
+            range: {
+                start: textDocument.positionAt(error.location[0].offset),
+                end: textDocument.positionAt(error.location[1].offset),
+            },
+            message: error.message,
+            source: "Lawtext"
+        };
+        diagnostics.push(diagnostic);
+    }
+
+    connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+};
+
+connection.onDidChangeWatchedFiles(() => {
+    connection.console.log("We received an file change event");
+});
+
+documents.listen(connection);
+
+connection.listen();
