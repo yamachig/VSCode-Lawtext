95fb6511bc3a9bc457fef13ab29b1ed27f07af18
yamachi <yamachig@users.noreply.github.com>
2022-04-17 13:10:04 -0700

    previewJsonELを修正

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -2,70 +2,113 @@ import * as vscode from "vscode";
 import { parse } from "lawtext/dist/src/parser/lawtext";
 import { analyze } from "lawtext/dist/src/analyzer";
 import * as renderer from "lawtext/dist/src/renderer";
-import { isJsonEL, JsonEL } from "lawtext/dist/src/node/el";
+import htmlCSS from "lawtext/dist/src/renderer/rules/htmlCSS";
+import { JsonEL } from "lawtext/dist/src/node/el";
 import path from "path";
 import { pictMimeDict } from "lawtext/dist/src/util";
 
-const renderHTML = (rawDocumentURI: string, el: JsonEL, convertUri?: (uri: vscode.Uri) => vscode.Uri) => {
+const renderHTMLFragment = (el: JsonEL, convertFigUri?: (src: string) => vscode.Uri) => {
     const getFigData = (src: string) => {
-        const documentURI = vscode.Uri.parse(rawDocumentURI);
-        const uri = vscode.Uri.joinPath(documentURI, "../", src);
-        const convertedUri = convertUri ? convertUri(uri) : uri;
         const ext = path.extname(src) as keyof typeof pictMimeDict;
         const type = ext in pictMimeDict ? pictMimeDict[ext] : "application/octet-stream";
-        return ext === ".pdf" ? null : { url: convertedUri.toString(), type };
+        const convertedUri = convertFigUri ? convertFigUri(src) : vscode.Uri.parse(src);
+        return { url: convertedUri.toString(), type };
     };
-    return renderer.renderHTML(
+    const fragment = renderer.renderHTML(
         el,
         {
             renderControlEL: true,
             getFigData,
+            renderPDFAsLink: true,
         });
+    return fragment;
 };
 
-const renderActiveEditor = (context: vscode.ExtensionContext, convertUri?: (uri: vscode.Uri) => vscode.Uri) => {
-    void context;
+const previewJsonEL = (context: vscode.ExtensionContext, el: JsonEL, rawDocumentURI?: string) => {
+    const panel = vscode.window.createWebviewPanel(
+        "lawtextPreview",
+        "Lawtext Preview",
+        vscode.ViewColumn.Two,
+        {
+            enableScripts: true,
+            enableFindWidget: true,
+        },
+    );
+
+    const convertFigUri = (rawDocumentURI !== undefined) ? (src: string) => {
+        const documentURI = vscode.Uri.parse(rawDocumentURI);
+        const uri = vscode.Uri.joinPath(documentURI, "../", src);
+        const convertedUri = path.extname(src) === ".pdf" ? uri : panel.webview.asWebviewUri(uri);
+        return convertedUri;
+    } : undefined;
+
+    const fragment = renderHTMLFragment(
+        el,
+        convertFigUri,
+    );
+
+    const html = /*html*/`\
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<style>${htmlCSS}</style>
+</head>
+<body>
+${fragment}
+<script>
+(() => {
+    const vscode = acquireVsCodeApi();
+    document.querySelectorAll("a[href]").forEach(a => {
+        a.addEventListener("click", e => {
+            e.preventDefault();
+            e.stopPropagation();
+            vscode.postMessage({
+                command: "openLink",
+                href: a.href,
+            });
+        });
+    });
+})();
+</script>
+</body>
+</html>
+`;
+
+    panel.webview.onDidReceiveMessage(
+        message => {
+            if (message.command === "openLink") {
+                const uri = vscode.Uri.parse(message.href);
+                vscode.commands.executeCommand("vscode.open", uri);
+            }
+        },
+        undefined,
+        context.subscriptions
+    );
+
+    panel.webview.html = html;
+};
+
+const showLawtextPreview = (context: vscode.ExtensionContext) => {
     const editor = vscode.window.activeTextEditor;
-    if (!editor) return "error";
-    if (!(editor.document.languageId === "lawtext")) {
-        return "Active editor doesn't show a Lawtext document.";
-    }
+    if (!editor) return;
+    if (editor.document.languageId !== "lawtext") return;
     const lawtext = editor.document.getText();
     const { value: law } = parse(lawtext);
     analyze(law);
-    const rendered = renderHTML(editor.document.uri.toString(), law, convertUri);
-    return rendered;
+    previewJsonEL(context, law, editor.document.uri.toString());
 };
 
 export const activate = (context: vscode.ExtensionContext) => {
-    context.subscriptions.push(
-        vscode.commands.registerCommand("extension.showLawtextPreview", () => {
-            const panel = vscode.window.createWebviewPanel(
-                "lawtextPreview",
-                "Lawtext Preview",
-                vscode.ViewColumn.Two,
-                {},
-            );
 
-            panel.webview.html = renderActiveEditor(context, uri => panel.webview.asWebviewUri(uri));
-        })
+    context.subscriptions.push(
+        vscode.commands.registerCommand("extension.showLawtextPreview", () => showLawtextPreview(context))
     );
 
     context.subscriptions.push(
-        vscode.commands.registerCommand("extension.previewJsonEL", (jsonEL: JsonEL, documentURI: string) => {
-            if (isJsonEL(jsonEL)) {
-                const panel = vscode.window.createWebviewPanel(
-                    "lawtextPreview",
-                    "Lawtext Preview",
-                    vscode.ViewColumn.Two,
-                    {},
-                );
-                panel.webview.asWebviewUri;
-
-                panel.webview.html = renderHTML(documentURI, jsonEL, uri => panel.webview.asWebviewUri(uri));
-            }
-        })
+        vscode.commands.registerCommand("extension.previewJsonEL", (...args: [el: JsonEL, rawDocumentURI: string]) => previewJsonEL(context, ...args))
     );
+
 };
 
 
