b88a2444a66b5e6856dcaf432a18f1e08e486fa4
yamachi <yamachig@users.noreply.github.com>
2022-04-19 23:46:06 -0700

    previewerを修正

--- client/src/command/previewEL.ts
+++ client/src/command/previewEL.ts
@@ -2,9 +2,11 @@ import * as vscode from "vscode";
 import * as renderer from "lawtext/dist/src/renderer";
 import htmlCSS from "lawtext/dist/src/renderer/rules/htmlCSS";
 import { EL } from "lawtext/dist/src/node/el";
+import * as std from "lawtext/dist/src/law/std";
 import path from "path";
 import { pictMimeDict } from "lawtext/dist/src/util";
 import _previewerScript from "../previewer/out/bundle.js.txt";
+import { PreviewerStateJSON } from "../previewer/src/stateInterface";
 
 const previewerScript = _previewerScript.replace(/<\/script>/g, "</ script>");
 
@@ -51,8 +53,23 @@ export interface PreviewELOptions {
     panel?: vscode.WebviewPanel,
 }
 
+const getFigDataMap = (el: EL | string, convertFigSrc: (src: string) => string) => {
+    const figDataMap: Record<string, {url: string, type: string}> = {};
+    if (typeof el === "string") return figDataMap;
+    if (std.isFig(el)) {
+        const src = el.attr.src;
+        const ext = path.extname(src) as keyof typeof pictMimeDict;
+        const type = ext in pictMimeDict ? pictMimeDict[ext] : "application/octet-stream";
+        figDataMap[src] = { url: convertFigSrc(src), type };
+    }
+    for (const child of el.children) {
+        Object.assign(figDataMap, getFigDataMap(child, convertFigSrc));
+    }
+    return figDataMap;
+};
+
 export const previewEL = (options: PreviewELOptions) => {
-    const { el } = options;
+    const { el, rawDocumentURI } = options;
 
     const panel = options.panel ?? vscode.window.createWebviewPanel(
         "lawtextPreview",
@@ -74,6 +91,7 @@ export const previewEL = (options: PreviewELOptions) => {
 <body>
 <div id="root"></div>
 <script>
+var exports = {};
 ${previewerScript}
 </script>
 </body>
@@ -81,16 +99,46 @@ ${previewerScript}
 `;
 
     panel.webview.html = html;
+
+    const els = [el.json(true, true)];
+
+    const figDataMap: Record<string, {url: string, type: string}> = {};
+
+    if (rawDocumentURI !== undefined) {
+        const convertFigSrc = (src: string) => {
+            const documentURI = vscode.Uri.parse(rawDocumentURI);
+            const uri = vscode.Uri.joinPath(documentURI, "../", src);
+            const convertedUri = path.extname(src) === ".pdf" ? uri : panel.webview.asWebviewUri(uri);
+            return convertedUri.toString();
+        };
+        Object.assign(figDataMap, getFigDataMap(el, convertFigSrc));
+    }
+
+    const htmlOptions: PreviewerStateJSON["htmlOptions"] = {
+        renderControlEL: true,
+        renderPDFAsLink: true,
+        figDataMap,
+    };
+
+    const previewerState: PreviewerStateJSON = {
+        els,
+        htmlOptions,
+        scrollOffset: 0,
+    };
+
+    const initialCenterOffset = (
+        (typeof options.initialCenterOffset === "number")
+            ? options.initialCenterOffset
+            : (typeof options.initialCenterOffset === "function")
+                ? options.initialCenterOffset()
+                : undefined
+    );
+
+    if (initialCenterOffset !== undefined) previewerState.scrollOffset = initialCenterOffset;
+
     panel.webview.postMessage({
         command: "setState",
-        state: {
-            els: [el.json(true)],
-            htmlOptions: {
-                renderControlEL: true,
-                renderPDFAsLink: true,
-                annotateLawtextRange: true,
-            }
-        },
+        state: previewerState,
     });
 };
 
--- client/src/previewer/src/index.tsx
+++ client/src/previewer/src/index.tsx
@@ -2,56 +2,197 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { HTMLAnyELs } from "lawtext/dist/src/renderer/rules/any";
 import htmlCSS from "lawtext/dist/src/renderer/rules/htmlCSS";
-import { HTMLOptions } from "lawtext/dist/src/renderer/common/html";
-import { EL, JsonEL, loadEl } from "lawtext/dist/src/node/el";
 import * as std from "lawtext/dist/src/law/std";
+import { PreviewerState, PreviewerStateJSON, toState, toStateJSON } from "./stateInterface";
 
-interface PreviewerStateJSON {
-    els: JsonEL[],
-    htmlOptions: HTMLOptions,
-}
-
-interface PreviewerState {
-    els: EL[],
-    htmlOptions: HTMLOptions,
-}
-
-const toStateJSON = (state: PreviewerState): PreviewerStateJSON => {
-    return {
-        els: state.els.map(el => el.json(true)),
-        htmlOptions: state.htmlOptions,
-    };
-};
-
-const toState = (stateJSON: PreviewerStateJSON): PreviewerState => {
-    return {
-        els: stateJSON.els.map(loadEl),
-        htmlOptions: stateJSON.htmlOptions,
-    };
-};
 
 const vscode = acquireVsCodeApi<PreviewerStateJSON>();
-const previousState = toState(vscode.getState() ?? { els: [], htmlOptions: {} });
+const previousState = toState(vscode.getState() ?? { els: [], htmlOptions: {}, scrollOffset: 0 });
+
+
+function throttle<TArgs extends unknown[]>(func: (...args: TArgs) => unknown, waitms: number) {
+    let timer: NodeJS.Timeout| undefined = undefined;
+    const lastArgsObj: {args?: TArgs} = {};
+    return (...args: TArgs) => {
+        lastArgsObj.args = args;
+        if (timer !== undefined) {
+            return;
+            // clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+            func(...(lastArgsObj.args as TArgs));
+            timer = undefined;
+            lastArgsObj.args = undefined;
+        }, waitms);
+    };
+}
+
+interface ActionCounter {
+    scroll: number;
+}
+
+const scrollToOffset = (offset: number) => {
+    const rangeInfo = {
+        start: {
+            d: null as number | null,
+            top: 0,
+        },
+        end: {
+            d: null as number | null,
+            top: null as number | null,
+        },
+    };
+    for (const el of Array.from(document.querySelectorAll("[data-lawtext_range]"))) {
+        const range = JSON.parse((el as HTMLElement).dataset["lawtext_range"] ?? "");
+        if (!range) return;
+        const relStart = range[0] - offset;
+        const relEnd = range[1] - offset;
+        const rect = el.getBoundingClientRect();
+        const [startTop, startD] = relEnd <= 0 ? [rect.bottom, relEnd] : [rect.top, relStart];
+        const [endTop, endD] = relStart >= 0 ? [rect.top, relStart] : [rect.bottom, relEnd];
+        if (startD <= 0 && ((rangeInfo.start.d === null) || (rangeInfo.start.d < startD))) {
+            rangeInfo.start.top = startTop;
+            rangeInfo.start.d = startD;
+        }
+        if (0 <= endD && ((rangeInfo.end.d === null) || (endD < rangeInfo.end.d))) {
+            rangeInfo.end.top = endTop;
+            rangeInfo.end.d = endD;
+        }
+    }
+    if ((rangeInfo.start.d === null) || (rangeInfo.end.d === null) || (rangeInfo.end.top === null)) return;
+    const r = -rangeInfo.start.d / (rangeInfo.end.d - rangeInfo.start.d);
+    const top = rangeInfo.start.top + r * (rangeInfo.end.top - rangeInfo.start.top);
+    const scrollEL = document.querySelector("html");
+    if (!scrollEL) return;
+    const scrollELRect = scrollEL.getBoundingClientRect();
+    const newScrollTop = (top - scrollELRect.top) - window.innerHeight / 2;
+    if (!Number.isFinite(newScrollTop)) return;
+
+    scrollEL.scrollTop = newScrollTop;
+};
+
+
+const getCenterOffset = (visibleELs: Set<HTMLElement>) => {
+    const scrollEL = document.querySelector("html");
+    if (!scrollEL) return;
+    const centerHeight = window.innerHeight / 2;
+    const rangeInfo = {
+        start: {
+            offset: 0,
+            y: null as number | null,
+        },
+        end: {
+            offset: null as number | null,
+            y: null as number | null,
+        },
+    };
+    for (const el of visibleELs) {
+        const range = JSON.parse(el.dataset["lawtext_range"] ?? "");
+        if (!range) continue;
+        const rect = el.getBoundingClientRect();
+        const relTop = rect.top - centerHeight;
+        const relBottom = rect.bottom - centerHeight;
+        const [startY, startOffset] = relBottom <= 0 ? [relBottom, range[1]] : [relTop, range[0]];
+        const [endY, endOffset] = relTop >= 0 ? [relTop, range[0]] : [relBottom, range[1]];
+        if (startY <= 0 && ((rangeInfo.start.y === null) || (rangeInfo.start.y < startY))) {
+            rangeInfo.start.offset = startOffset;
+            rangeInfo.start.y = startY;
+        }
+        if (0 <= endY && ((rangeInfo.end.y === null) || (endY < rangeInfo.end.y))) {
+            rangeInfo.end.offset = endOffset;
+            rangeInfo.end.y = endY;
+        }
+    }
+    if ((rangeInfo.start.y === null) || (rangeInfo.end.y === null) || (rangeInfo.end.offset === null)) return;
+    const r = -rangeInfo.start.y / (rangeInfo.end.y - rangeInfo.start.y);
+    const offset = Math.round(rangeInfo.start.offset + r * (rangeInfo.end.offset - rangeInfo.start.offset));
+    if (!Number.isFinite(offset)) return;
+    return offset;
+};
 
 const App = () => {
     const [state, _setState] = React.useState<PreviewerState>(previousState);
-    const setState = React.useCallback((newState: PreviewerState) => {
-        vscode.setState(toStateJSON(newState));
-        _setState(newState);
-    }, []);
     const { els, htmlOptions } = state;
+
+    const visibleELs = React.useMemo(() => new Set<HTMLElement>(), []);
+
+    const counter = React.useMemo<ActionCounter>(() => ({ scroll: 0 }), []);
+
+    const setState = React.useCallback((newStateFunc: (prevState: PreviewerState) => PreviewerState) => {
+        _setState(prevState => {
+            const newState = newStateFunc(prevState);
+            vscode.setState(toStateJSON(newState));
+            return newState;
+        });
+    }, []);
+
+    const setStateJSON = React.useCallback((newStateJSONFunc: (prevStateJSON: PreviewerStateJSON) => PreviewerStateJSON) => {
+        _setState(prevState => {
+            const newStateJSON = newStateJSONFunc(prevState);
+            vscode.setState(newStateJSON);
+            return toState(newStateJSON);
+        });
+    }, []);
+
+
+    React.useEffect(() => {
+        const observerCallback: IntersectionObserverCallback = (entries) => {
+            for (const entry of entries) {
+                if (entry.isIntersecting) {
+                    visibleELs.add(entry.target as HTMLElement);
+                } else {
+                    visibleELs.delete(entry.target as HTMLElement);
+                }
+            }
+        };
+        const observer = new IntersectionObserver(observerCallback);
+        for (const el of Array.from(document.querySelectorAll("[data-lawtext_range]"))) {
+            observer.observe(el);
+        }
+        return () => {
+            observer.disconnect();
+            visibleELs.clear();
+        };
+    }, [state.els, visibleELs]);
 
     React.useEffect(() => {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const onMessage = (event: any) => {
             if (event.data?.command === "setState") {
-                const newStateJSON = event.data.state as PreviewerStateJSON;
-                setState(toState(newStateJSON));
+                setStateJSON(() => event.data.state as PreviewerStateJSON);
             }
         };
         window.addEventListener("message", onMessage);
         return () => window.removeEventListener("message", onMessage);
-    }, [setState]);
+    }, [setStateJSON]);
+
+    React.useEffect(() => {
+        console.log(`scrollOffset changed: ${state.scrollOffset}`);
+        scrollToOffset(state.scrollOffset);
+    }, [state.scrollOffset]);
+
+    React.useEffect(() => {
+        const onScroll = () => {
+            if (counter.scroll > 0) {
+                counter.scroll--;
+            } else {
+                counter.scroll++;
+                const offset = getCenterOffset(visibleELs);
+                if (typeof offset === "number") {
+                    setState(prevState => ({ ...prevState, scrollOffset: offset }));
+                } else {
+                    counter.scroll--;
+                }
+            }
+        };
+        const throttleOnScroll = throttle(onScroll, 100);
+        window.addEventListener("scroll", throttleOnScroll);
+        window.addEventListener("resize", throttleOnScroll);
+        return () => {
+            window.removeEventListener("scroll", throttleOnScroll);
+            window.removeEventListener("resize", throttleOnScroll);
+        };
+    }, [counter.scroll, setState, visibleELs]);
 
     return (<>
         <style>
@@ -60,7 +201,10 @@ const App = () => {
         <HTMLAnyELs
             els={els as (string | std.StdEL | std.__EL)[]}
             indent={0}
-            {...{ htmlOptions }}
+            {...{ htmlOptions: {
+                ...htmlOptions,
+                annotateLawtextRange: true,
+            } }}
         />
     </>);
 };
